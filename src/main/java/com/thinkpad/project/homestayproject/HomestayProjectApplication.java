3f196284d6f814a0680b5991002aae9276ef19ab
dat <datdta95@gmail.com>
2018-12-11 14:53:37 +0700

    static file

---
+++ src/main/java/com/thinkpad/project/homestayproject/HomestayProjectApplication.java
@@ -0,0 +1,12 @@
+package com.thinkpad.project.homestayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomestayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomestayProjectApplication.class, args);
+	}
+}
